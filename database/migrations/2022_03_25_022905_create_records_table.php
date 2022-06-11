f61b023018596cd14d839032aad79a20f52cde12
Dmity Belousov <da_belousov@list.ru>
2022-06-11 12:01:25 +0800

    Создание миграции таблиц

---
+++ database/migrations/2022_03_25_022905_create_records_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->bigInteger('service_id')->unsigned()->nullable();
+
+            $table->string('title')->nullable();
+            $table->text('comment')->nullable();
+            $table->integer('status')->unsigned();
+            $table->dateTime('start');
+            $table->dateTime('end');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
